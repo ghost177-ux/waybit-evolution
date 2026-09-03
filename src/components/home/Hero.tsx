067416a1632f85b7fe0e1526df8fc318f5ff1ac4
ghost177-ux <ghosthappy177@gmail.com>
2026-09-03 16:13:57 -0300

    Update Hero.tsx

--- src/components/home/Hero.tsx
+++ src/components/home/Hero.tsx
@@ -53,7 +53,7 @@ export function Hero() {
             className="absolute inset-y-0 left-1/2 -right-[50vw] z-0 hidden transition-colors duration-700 lg:block"
             style={{
               backgroundColor: theme.color,
-              clipPath: "polygon(25% 0, 100% 0, 100% 100%, 25% 100%, 0 50%)"
+              clipPath: "polygon(74% 0%, 100% 0%, 100% 100%, 74% 100%, 53% 50%)"
             }}
           />
 
