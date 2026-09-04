aa4ab6a4420f97ec86004af5bcea521f0940ced9
ghost177-ux <ghosthappy177@gmail.com>
2026-09-04 17:18:28 -0300

    Add MetricasDominus component to main route

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -10,6 +10,7 @@ import { Cases } from "@/components/home/Cases";
 import { Support } from "@/components/home/Support";
 import { Solutions } from "@/components/home/Solutions";
 import { Footer } from "@/components/home/Footer";
+import { MetricasDominus } from "@/components/home/metricasDominus";
 
 export const Route = createFileRoute("/")({
   head: () => ({
@@ -47,6 +48,7 @@ function Home() {
         <Cases />
         <Support />
         <Solutions />
+        <MetricasDominus/>
       </main>
       <Footer />
     </div>
