0fa6ab5eff5c733172a15e456b7f451ffd9289a9
ghost177-ux <ghosthappy177@gmail.com>
2026-09-10 14:12:03 -0300

    Change main image height from 50% to 75%

--- src/components/home/Apresentacao.tsx
+++ src/components/home/Apresentacao.tsx
@@ -20,7 +20,7 @@ export default function Apresentacao() {
         />
       </div>
 
-      {/* Imagem principal ocupando 50% de cima para baixo */}
+      {/* Imagem principal ocupando 75% de cima para baixo */}
       <div className="relative mx-auto w-[75%] h-[75%] flex items-center justify-center">
         <img
           src={apresent.url}
